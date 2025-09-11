8d78d07ca0d23b04eff02a4fb294e768cb226e0e
abbigailf <anelson6@byupathway.edu>
2025-09-11 12:26:08 -0600

    book of mormon commit

---
+++ week02/scripts/bom.js
@@ -0,0 +1,25 @@
+const input = document.querySelector('#favchap');
+const button = document.querySelector('button');
+const list = document.querySelector('#list');
+
+button.addEventListener('click', () => {
+    if (input.value.trim() === '') return;
+
+    const li = document.createElement('li');
+    li.textContent = input.value;
+
+    const deleteButton = document.createElement('button');
+    deleteButton.textContent = '❌';
+    deleteButton.setAttribute('aria-label', `Remove ${input.value}`);
+
+    li.append(deleteButton);
+
+    list.append(li);
+
+    input.value = '';
+    input.focus();
+
+    deleteButton.addEventListener('click', () => {
+        list.removeChild(li);
+    });
+});
