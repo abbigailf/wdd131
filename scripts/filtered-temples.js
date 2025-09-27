6ae8929b551b07cc3e351da7ee8fba252b4b9b9b
abbigailf <anelson6@byupathway.edu>
2025-09-27 17:28:17 -0600

    fix

--- scripts/filtered-temples.js
+++ scripts/filtered-temples.js
@@ -4,70 +4,70 @@ const temples = [
         location: "Aba, Nigeria",
         dedicated: "2005, August, 7",
         area: 11500,
-        imageUrl: "http://127.0.0.1:5500/images/aba-nigeria-temple.jpg"
+        imageUrl: "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/aba-nigeria/400x250/aba-nigeria-temple-lds-273999-wallpaper.jpg"
     },
     {
         templeName: "Manti Utah",
         location: "Manti, Utah, United States",
         dedicated: "1888, May, 21",
         area: 74792,
-        imageUrl: "http://127.0.0.1:5500/images/manti-temple.jpg"
+        imageUrl: "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/manti-utah/400x250/manti-temple-768192-wallpaper.jpg"
     },
     {
         templeName: "Payson Utah",
         location: "Payson, Utah, United States",
         dedicated: "2015, June, 7",
         area: 96630,
-        imageUrl: "http://127.0.0.1:5500/images/payson-utah-temple.jpg"
+        imageUrl: "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/payson-utah/400x225/payson-utah-temple-exterior-1416671-wallpaper.jpg"
     },
     {
         templeName: "Yigo Guam",
         location: "Yigo, Guam",
         dedicated: "2020, May, 2",
         area: 6861,
-        imageUrl: "http://127.0.0.1:5500/images/yigo_guam_temple.jpg"
+        imageUrl: "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/yigo-guam/400x250/yigo_guam_temple_2.jpg"
     },
     {
         templeName: "Washington D.C.",
         location: "Kensington, Maryland, United States",
         dedicated: "1974, November, 19",
         area: 156558,
-        imageUrl: "http://127.0.0.1:5500/images/washington_dc_temple.jpeg"
+        imageUrl: "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/washington-dc/400x250/washington_dc_temple-exterior-2.jpeg"
     },
     {
         templeName: "Lima Perú",
         location: "Lima, Perú",
         dedicated: "1986, January, 10",
         area: 9600,
-        imageUrl: "http://127.0.0.1:5500/images/lima-peru-temple.jpg"
+        imageUrl: "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/lima-peru/400x250/lima-peru-temple-evening-1075606-wallpaper.jpg"
     },
     {
         templeName: "Mexico City Mexico",
         location: "Mexico City, Mexico",
         dedicated: "1983, December, 2",
         area: 116642,
-        imageUrl: "http://127.0.0.1:5500/images/mexico-city-temple.jpg"
+        imageUrl: "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/mexico-city-mexico/400x250/mexico-city-temple-exterior-1518361-wallpaper.jpg"
     },
     {
         templeName: "Okinawa Japan",
         location: "Okinawa, Japan",
         dedicated: "2023, November, 12",
         area: 12437,
-        imageUrl: "http://127.0.0.1:5500/images/okinawa-japan.jpg"
+        imageUrl: "https://churchofjesuschristtemples.org/assets/img/temples/okinawa-japan-temple/okinawa-japan-temple-40844.jpg"
     },
     {
         templeName: "London England",
         location: "Surrey, England",
         dedicated: "1958, September, 7",
         area: 42652,
-        imageUrl: "http://127.0.0.1:5500/images/london-england.jpg"
+        imageUrl: "https://churchofjesuschristtemples.org/assets/img/temples/london-england-temple/london-england-temple-63059.jpg"
     },
     {
         templeName: "Brigham City Utah",
         location: "Brigham City, Utah, United States",
         dedicated: "2012, September, 23",
         area: 36000,
-        imageUrl: "http://127.0.0.1:5500/images/brigham-city-utah-temple.jpg"
+        imageUrl: "https://churchofjesuschristtemples.org/assets/img/temples/brigham-city-utah-temple/brigham-city-utah-temple-39612.jpg"
     }
 ];
 
